258db8e5994c3658ec3d621b4351aabf53cd89d0
Mialaniz <michaelalaniz76@gmail.com>
2024-12-25 23:01:41 -0800

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,47 +6,30 @@
 import Link from "next/link"
 import {Box, Container, Grid, Heading, Section, Text} from "@radix-ui/themes";
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
     const logos = ['att', 'google', 'meta', 'microsoft'];
 
     return (
         <main>
+            <Navbar/>
             <Section className="w-full py-12 md:py-24 lg:py-32 px-6">
                 <Container>
-                    <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+                    <div className="grid lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
                         <Image
-                            src={"/images/cover.jpg"}
-                            width="550"
-                            height="550"
+                            src={"/images/boxing.jpg"}
+                            width="3000"
+                            height="300"
                             alt="Hero"
-                            className="mx-auto grayscale aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
+                            className="mx-auto aspect-video overflow-hidden rounded-xl object-cover"
                         />
                         <div className="flex flex-col justify-center space-y-4">
                             <div>
-                                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                                    The complete platform to keep your money up
-                                </h1>
-                                <Text as={'p'} className="my-4 max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                                <Text as={'p'} className="my-4 max-w-[600px] text-gray-500 md:text-xl dark:text-gray-700">
                                     Give your team the education that they deserve. With thousands of courses,
                                     you can elevate your mind. Get ya money up not ya funny up.
                                 </Text>
-                            </div>
-                            <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                                <Link
-                                    href="/login"
-                                    className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                                    prefetch={false}
-                                >
-                                    Get Started
-                                </Link>
-                                <Link
-                                    href="#"
-                                    className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                                    prefetch={false}
-                                >
-                                    Contact Sales
-                                </Link>
                             </div>
                         </div>
                     </div>
@@ -56,7 +39,7 @@ export default function Page() {
                 <Container>
                     <div className="flex flex-col items-center justify-center space-y-4 text-center">
                         <div className={'mb-8'}>
-                            <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
+                            <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-700">
                                 New Courses Every Week
                             </div>
                             <Heading as={'h2'} className="text-3xl font-bold tracking-tighter sm:text-5xl mb-4">Maxxing Learning Stats.</Heading>
@@ -89,7 +72,7 @@ export default function Page() {
                     </div>
                 </Container>
             </Section>
-            <Section className="w-full px-6 py-12 md:py-24 lg:py-32 bg-gray-100 bg-[#3361AC]">
+            <Section className="w-full px-6 py-12 md:py-24 lg:py-32 bg-gray-100 bg-[#0e6aff]">
                 <Container className="grid items-center justify-center gap-4 px-4 text-center md:px-6">
                     <Box className="space-y-3">
                         <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box, Container, Grid, Heading, Section, Text} from "@radix-ui/themes";
+
+
+export default function () {
+  return (
+    <div className="w-full h-10 bg-[#0e6aff]">
+        <Text className="text-xl pl-10 pt-14">
+            Get2Boxing
+        </Text>
+    </div>
+  )
+}
