793114135a252d4d8957dc51253e54df04d968d4
Michael Alaniz <michaelalaniz@Michaels-MacBook-Pro.local>
2025-01-09 15:03:21 -0800

    forgot to save

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,20 +12,20 @@ interface Product {
 
 export default function ProductCard({product}: {product: Product}) {
   return (
-    <div className="bg-white rounded-lg shadow-md">
-        <AspectRatio.Root ratio={1}>
-            <Image
-                src={product.image}
-                alt={product.name}
-            />
-        </AspectRatio.Root>
-        <div className="p-4">
-            <h3 className="text-lg font-semibold text-blue-900">{product.name}</h3>
-            <p className="text-yellow-600 font-bold mt-2">${product.price.toFixed(2)}</p>
-            <Button className=" rounded-md w-full mt-4 bg-blue-600 text-yellow-300 hover:bg-blue-700">
-            Add to Cart
-            </Button>
+        <div className="bg-white rounded-lg shadow-md">
+            <AspectRatio.Root ratio={1}>
+                <Image
+                    src={product.image}
+                    alt={product.name}
+                />
+            </AspectRatio.Root>
+            <div className="p-4">
+                <h3 className="text-lg font-semibold text-blue-900">{product.name}</h3>
+                <p className="text-yellow-600 font-bold mt-2">${product.price.toFixed(2)}</p>
+                <Button className=" rounded-md w-full mt-4 bg-blue-600 text-yellow-300 hover:bg-blue-700">
+                Add to Cart
+                </Button>
+            </div>
         </div>
-    </div>
   )
 }
