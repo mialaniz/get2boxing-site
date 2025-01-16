05ba33a32338e73c1f50c0acb68c73b4d25391c5
Mialaniz <michaelalaniz76@gmail.com>
2025-01-16 10:28:29 -0800

    new folders

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-primary text-white py-6">
-        <div className="container mx-auto px-4 text-center">
-          <p>&copy; {new Date().getFullYear()} Get2Boxing. All rights reserved.</p>
-        </div>
-      </footer>
-    )
-  }
