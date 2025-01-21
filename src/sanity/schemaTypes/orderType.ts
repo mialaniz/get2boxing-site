95386cbd9663c9cf7a16418c3d6ac992fa52bb14
Mialaniz <michaelalaniz76@gmail.com>
2025-01-20 17:24:59 -0800

    finished the schema

--- src/sanity/schemaTypes/orderType.ts
+++ src/sanity/schemaTypes/orderType.ts
@@ -52,7 +52,49 @@ export const orderType = defineType({
             title: 'Total Price',
             type: 'number',
             validation: (Rule) => Rule.required() 
-
-        })
+        }),
+        defineField({
+            name: 'currency',
+            title: 'Currency',
+            type: 'string',
+            validation: (Rule) => Rule.required()
+        }),
+        defineField({
+            name: 'status',
+            title: 'Order Status',
+            type: 'string',
+            options: {
+                list: [
+                    {title: "Pending", value: "pending"},
+                    {title: "Paid", value: "paid"},
+                    {title: "Shipped", value: "shipped"},
+                    {title: "Delivered", value: "delivered"},
+                    {title: "Cancelled", value: "cancelled"}
+                ],
+            }
+        }), 
+        defineField({
+            name: 'orderDate',
+            title: 'Order Date',
+            type: 'datetime',
+            validation: (Rule) => Rule.required()
+        }),
     ],
+    preview:{
+        select:{
+            name: 'customerName',
+            amount: 'totalPrice',
+            currency: 'currency',
+            orderId: 'orderNumber',
+            email: 'email',
+        },
+        prepare(select) {
+            const orderIdSnippet = `${select.orderId.slice(0,5)}...${(select.orderId.slice(-5))}`;
+            return{
+                title: `${select.name}`,
+                subtitle: `${select.amount} ${select.currency}, ${select.email}`,
+                media: BasketIcon,
+            };
+        }
+    }
 });
--- src/sanity/schemaTypes/productType.ts
+++ src/sanity/schemaTypes/productType.ts
@@ -53,7 +53,4 @@ export const productType = defineType({
             }
         }
     },
-
-
-
 });
