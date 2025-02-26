f03536f0b6d9beff5deb25f0ee37b4bde06d94e1
Michael <michaelalaniz76@gmail.com>
2025-02-26 12:54:19 -0800

    checkout change

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { NextResponse, NextRequest } from "next/server";
 
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!)
 
 export async function POST(req: NextRequest, res: NextResponse) {
 
@@ -10,7 +10,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     const sig = req.headers.get("Stripe-Signature");
     try {
-        let event = stripe.webhooks.constructEvent(payload, sig!, process.env.NEXT_PUBLIC_STRIPE_WEBHOOKS);
+        let event = stripe.webhooks.constructEvent(payload, sig!, process.env.NEXT_PUBLIC_STRIPE_WEBHOOKS!);
         console.log("event: ", event.type);
 
         return NextResponse.json({status: "success", event: event.type});
