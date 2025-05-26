ea43883b0f2acc59141957ba7137541118e27b75
Michael <michaelalaniz76@gmail.com>
2025-05-26 15:03:46 -0700

    new

--- src/components/wrappers/Add-To-Cart-Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import AddToCartButton from "../Add-To-Cart-Button";
-
-export default async function AddToCartWrapper({product, boughtPrice} : {product: any, boughtPrice: number}) {
-    
-    return (
-        <AddToCartButton product={product} boughtProduct={boughtPrice}/>
-    )
-
-}
